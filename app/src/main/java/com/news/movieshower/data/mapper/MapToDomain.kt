bcab1c90eb03bf68820e1cc89ad939752038fe46
Mnacakanyan <n.mnacakanyan13@gmail.com>
2025-11-21 15:07:10 +0400

    Remove unused mapping function

--- app/src/main/java/com/news/movieshower/data/mapper/MapToDomain.kt
+++ app/src/main/java/com/news/movieshower/data/mapper/MapToDomain.kt
@@ -1,18 +1,8 @@
 package com.news.movieshower.data.mapper
 
 import com.news.movieshower.data.model.Result
-import com.news.movieshower.data.model.WeeklyTrendingMoviesResponse
 import com.news.movieshower.domain.model.ResultDomainModel
-import com.news.movieshower.domain.model.WeeklyTrendingMoviesDataModel
 
-fun WeeklyTrendingMoviesResponse.toDomain() : WeeklyTrendingMoviesDataModel{
-    return WeeklyTrendingMoviesDataModel(
-        page = page,
-        results = results.map { it.toDomain() },
-        totalPages = total_pages,
-        totalResults = total_results
-    )
-}
 
 fun Result.toDomain(): ResultDomainModel {
     return ResultDomainModel(
